72178dc1e305552162e42288944c1fa29a4d5f30
khandya1 <63929895+khandya1@users.noreply.github.com>
2021-04-12 16:12:08 +0530

    folder creation updated

--- src/main/java/FileConversion.java
+++ src/main/java/FileConversion.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.io.File;
 import java.io.FileInputStream;
@@ -8,30 +9,42 @@ public class FileConversion
 {
     public void uploadfile(String attribute_name) {
     }
-    public void createFile(String attribute_name) throws IOException {
+    public void createFile() throws IOException {
         String csvSplitter = ",";
-        BufferedReader br = new BufferedReader(new FileReader("Test_Columns_Seperate.csv"));
+        BufferedReader br = new BufferedReader(new FileReader("src/main/resources/Test_Columns_Separate.csv"));
         PrintWriter pw;
         String line;
         line=br.readLine();
+        System.out.println("**********************");
         String[] header=line.split(csvSplitter);
-        String csv_name=header[1];////message to anshul 1 is index of column, while loop for rest of the columns
-        pw = new PrintWriter(new File(csv_name+".csv"));
-        //StringBuffer csvHeader = new StringBuffer("");
-        StringBuffer csvData = new StringBuffer("");
-        while ((line = br.readLine()) != null) {
-            String[] cols = line.split(csvSplitter);
-            csvData.append(cols[1]);
-            csvData.append('\n');
+
+        System.out.println(Arrays.toString(header));
+        int n = header.length;
+        System.out.println("Total headers is "+n);
+        int i = 0;
+        while(i++<n){
+            System.out.println(i);
+            String csv_name=header[i];////message to anshul 1 is index of column, while loop for rest of the columns
+            System.out.println(csv_name);
+            pw = new PrintWriter(new File("src/main/resources/abc/in/"+csv_name+".csv"));
+            System.out.println("*************");
+            //StringBuffer csvHeader = new StringBuffer("");
+            StringBuffer csvData = new StringBuffer("");
+            System.out.println("/////////////////////");
+            while ((line = br.readLine()) != null) {
+                String[] cols = line.split(csvSplitter);
+                System.out.println(cols[i]);
+                csvData.append(cols[i]);
+                csvData.append('\n');
+            }
+            //pw.write(csvHeader.toString());
+            System.out.println(csvData);
+            pw.write(csvData.toString());
+            pw.close();
         }
-        //pw.write(csvHeader.toString());
-        pw.write(csvData.toString());
-        pw.close();
+
     }
-
-
-
-
 }
+
 
 
--- src/main/java/FolderCreation.java
+++ src/main/java/FolderCreation.java
@@ -1,37 +1,46 @@
 import java.io.File;
+import java.io.IOException;
+import java.util.LinkedHashSet;
 import java.util.Scanner;
 import java.util.Set;
 
 public class FolderCreation {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Enter the database name");
         String dbname = sc.next();
         File file = new File("src/main/resources/"+dbname);
         boolean bool = file.mkdir();
-        Set<String> setofDimensionName = null;
+        Set<String> setofDimensionName = new LinkedHashSet<String>();
         System.out.println("Enter number of dimensions");
         int count_dimensional_table = sc.nextInt();
-        while(count_dimensional_table--)
+        while(count_dimensional_table-->0)
         {
             System.out.println("Enter Dimensional table name");
             String dimension = sc.next();
+            System.out.println(dimension);
+            assert false;
             setofDimensionName.add(dimension);
+            System.out.println("********************");
             file = new File("src/main/resources/"+dbname+"/"+dimension);
+            System.out.println(file);
             bool = file.mkdir();
 
-            System.out.println("Enter number of attributes");
-            int attributes = sc.nextInt();
-            while(attributes--)
-            {
-                FileConversion fc = new FileConversion();
-                String attribute_name = sc.next();
-                fc.uploadfile(attribute_name);
+            FileConversion fc = new FileConversion();
+            fc.createFile();
+//            System.out.println("Enter number of attributes");
+//            int attributes = sc.nextInt();
+//            while(attributes-->0)
+//            {
+//                FileConversion fc = new FileConversion();
+//                String attribute_name = sc.next();
+//                //attribute_name = "src/main/resources/"+dbname+"/"+dimension + "/"+attribute_name;
+//                fc.createFile();
+//            }
+     }
 
-            }
-       }
         LatticeCreation latticeCreation = new LatticeCreation();
         Set<Set<String>> dimensionPower= latticeCreation.generatePowerSet(setofDimensionName);
         Set<String > dimensionNames=latticeCreation.generateLatticeName(dimensionPower);
